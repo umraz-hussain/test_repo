5f8815f4036b36ef9cc5fef1f965add7b1258c84
umrazhussain <47004577+umrazhussain@users.noreply.github.com>
2021-04-13 17:06:54 +0530

    This is 2nd test file for branch creation

---
+++ test2.py
@@ -0,0 +1,12 @@
+print("HI there, Umraz Hussain, im just testing git here.. Thank you!")
+print("lets add some more code here to see changes in git..")
+n = input("Enter your option to get discount 1 or 2 ")
+if n == "1":
+    print("your discount is: 10%")
+else:
+    print("your discount is: 8%")
+#def sum(n1,n2):
+#    return n1+n2
+#n1 = int(input('Enter first number: ')
+#n2 = int(input('Enter Second number: ')
+#print(f'the sum of {n11} and {n12} is: ',sum(n11,n12))
