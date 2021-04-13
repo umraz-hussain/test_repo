9fab532eb21972cf46064718f58d21c1e23752dd
umrazhussain <47004577+umrazhussain@users.noreply.github.com>
2021-04-13 16:28:27 +0530

    Add some more code to to check modd of the person

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("HI there, Umraz Hussain, im just testing git here.. Thank you!")
+print("lets add some more code here to see changes in git..")
+n = input("Enter your mood: ")
+if n == "sad":
+    print("Play some game..")
+else:
+    print("Listen to some songs...")
+#def sum(n1,n2):
+#    return n1+n2
+#n1 = int(input('Enter first number: ')
+#n2 = int(input('Enter Second number: ')
+#print(f'the sum of {n11} and {n12} is: ',sum(n11,n12))
