465b495f91e7dc85135ee984bffcced244addf47
umrazhussain <47004577+umrazhussain@users.noreply.github.com>
2021-04-13 16:46:45 +0530

    added some more code to perform undo operations

--- test.py
+++ test.py
@@ -5,8 +5,14 @@ if n == "sad":
     print("Play some game..")
 else:
     print("Listen to some songs...")
+
+
+
 #def sum(n1,n2):
 #    return n1+n2
 #n1 = int(input('Enter first number: ')
 #n2 = int(input('Enter Second number: ')
 #print(f'the sum of {n11} and {n12} is: ',sum(n11,n12))
+
+
+print("Add sooo amnu print stmt lets undo it....")
